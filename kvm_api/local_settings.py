b49ffe448c21a815b3df4389dee8382b7bc34489
Parker Lusk <parkerclusk@gmail.com>
2014-03-02 20:50:16 -0700

    local settings; db setup

---
+++ kvm_api/local_settings.py
@@ -0,0 +1,17 @@
+DATABASES = {
+'default': {
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kvm',
+        'USER': 'plusk01',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': 'db.db',
+    #     'OPTIONS': {
+    #     	'timeout': 120
+    #     }
+    # }
+}
--- kvm_api/settings.py
+++ kvm_api/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+from local_settings import *
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -59,12 +61,12 @@ WSGI_APPLICATION = 'kvm_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
