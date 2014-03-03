43cf5b29f3fe539e337cdb949feb57ee24b3107a
Parker Lusk <parkerclusk@gmail.com>
2014-03-02 18:01:53 -0700

    backend app; rest framework

--- kvm_api/settings.py
+++ kvm_api/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'backend',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,3 +84,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
