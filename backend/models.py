c867736bd3bbb2379d4a40ea127f3e7ed51cf1e3
Parker Lusk <parkerclusk@gmail.com>
2014-03-09 08:26:09 -0600

    working on models

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Application(models.Model):
+	MALE = 'M'
+	FEMALE = 'F'
+	GENDER_CHOICES = (
+		(MALE, 'Male'),
+		(FEMALE, 'Female'),
+	)
+
+	# Personal Information
+	first_name = models.CharField(max_length=75)
+	last_name = models.CharField(max_length=75)
+	birthday = models.DateField()
+	gender = models.CharField(choices=GENDER_CHOICES)
+	email = models.CharField(max_length=75)
+	mobile = models.CharField(max_length=20)
+	father = models.CharField(max_length=150)
+	present_address = models.CharField(max_length=255)
+	permanent_address = models.CharField(max_length=255)
+
+	# Financial Information
+	parents_income = models.ForeignKey('Income')
+	mother_job = models.CharField(max_length=100)
+	father_job = models.CharField(max_length=100)
